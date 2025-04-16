eb9541890a692fe91462dfee8493412e6070a987
Minjae Kim <minjaekim9929@gmail.com>
2025-04-15 23:58:49 -0400

    Add feature card components in feature cards component

--- src/components/FeatureCards/FeatureCards.tsx
+++ src/components/FeatureCards/FeatureCards.tsx
@@ -1,6 +1,34 @@
 import React from 'react';
 import './FeatureCards.scss';
+import FeatureCard from './FeatureCard/FeatureCard';
 
-function FeatureCards() {}
+function FeatureCards() {
+	return (
+		<div className="featureCards">
+			<div className="featureCard1">
+				<FeatureCard
+					img="/images/friend-icon.png"
+					title="Find Study Partners"
+					description="Match with students in your major or courses to study together."
+				/>
+			</div>
+
+			<div className="featureCard2">
+				<FeatureCard
+					img="/images/calendar-icon.png"
+					title="Create or Join Groups"
+					description="Start your own study group or join one that fits your schedule."
+				/>
+			</div>
+			<div className="featureCard3">
+				<FeatureCard
+					img="/images/chat-icon.png"
+					title="Stay connected"
+					description="Coordinate sessions and build a community with peers"
+				/>
+			</div>
+		</div>
+	);
+}
 
 export default FeatureCards;
