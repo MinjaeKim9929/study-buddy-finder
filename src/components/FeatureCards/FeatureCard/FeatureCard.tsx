63b3863034ff36bde0f4f570bbc88506365bcb87
Minjae Kim <minjaekim9929@gmail.com>
2025-04-15 21:25:54 -0400

    Add Feature Card content

--- src/components/FeatureCards/FeatureCard/FeatureCard.tsx
+++ src/components/FeatureCards/FeatureCard/FeatureCard.tsx
@@ -1,6 +1,28 @@
 import React from 'react';
 import './FeatureCard.scss';
 
-function FeatureCard() {}
+function FeatureCard({
+	img,
+	title,
+	description,
+	className = '',
+}: {
+	img: string;
+	title: string;
+	description: string;
+	className?: string;
+}) {
+	return (
+		<div className={`featureCard ${className}`}>
+			<div className="featureCard-image">
+				<img src={img} alt="Feature card" />
+			</div>
+			<div className="featureCard-text">
+				<div className="featureCard-title">{title}</div>
+				<div className="featureCard-description">{description}</div>
+			</div>
+		</div>
+	);
+}
 
 export default FeatureCard;
