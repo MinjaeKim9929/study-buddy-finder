f812fc723543053b45c6edc1d4fffe1053712937
Minjae Kim <minjaekim9929@gmail.com>
2025-04-16 03:20:07 -0400

    Add User profile form folder and files.

---
+++ src/components/Forms/UserProfileForm/UserProfileForm.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './UserProfileForm.scss';
+
+function UserProfileForm() {
+	return (
+		<div>
+			<div>User Profile Form</div>
+		</div>
+	);
+}
+
+export default UserProfileForm;
--- src/pages/Signup/SignUpPage.tsx
+++ src/pages/Signup/SignUpPage.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import RegisterForm from '../../components/Forms/RegisterForm/RegisterForm';
+import UserProfileForm from '../../components/Forms/UserProfileForm/UserProfileForm';
 
 function SignUpPage() {
 	return (
 		<div className="signUpPage">
 			<RegisterForm />
+			<UserProfileForm />
 		</div>
 	);
 }
