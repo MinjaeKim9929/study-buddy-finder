410c8c1f5090c8e425cf6232dae3596df44ba138
Minjae Kim <minjaekim9929@gmail.com>
2025-04-19 15:30:56 -0400

    Create group page

--- src/pages/CreateGroup/CreateGroupPage.tsx
+++ src/pages/CreateGroup/CreateGroupPage.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import CreateGroupForm from '../../components/Forms/CreateGroupForm/CreateGroupForm';
+import { db } from '../../config/firebaseConfig';
+import { collection, addDoc, Timestamp } from 'firebase/firestore';
+import { useAuth } from '../../config/AuthContext';
 
 function CreateGroupPage() {
-	return (
-		<div>
-			<div>CreateGroupPage</div>
-		</div>
-	);
+	const { currentUser } = useAuth();
+
+	const handleCreateGroup = async (groupData: any) => {
+		try {
+			await addDoc(collection(db, 'groups'), {
+				...groupData,
+				creator: currentUser?.uid,
+				members: [currentUser?.uid],
+				createdAt: Timestamp.now(),
+			});
+			alert('Group created successfully!');
+		} catch (err) {
+			console.error('Failed to create group:', err);
+			alert('Error creating group. Try again.');
+		}
+	};
+	return <CreateGroupForm onSubmit={handleCreateGroup} />;
 }
 
 export default CreateGroupPage;
